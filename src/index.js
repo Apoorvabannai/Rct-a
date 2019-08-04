02df4e49a8061e8843767e3825c36422b1985541
bapoorvasai <bapoorvasai@gmail.com>
2019-08-04 01:24:33 -0600

    Lint

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM  from 'react-dom';
 
 class App extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
 
-    this.state = {lat: null};
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState(lat: position),
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
-    )
+    );
   };
 
   render() {
